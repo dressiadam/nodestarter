e68322758a8915a43a492078abe70bc829981870
desca <david.klausz@gmail.com>
2015-03-29 11:44:28 +0200

    MongoDB out

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var config = require('./config'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
 	session = require('express-session'),
-	mongoStore = require('connect-mongo')(session),
 	mongoose = require('mongoose'),
     http = require('http'),
 	app = express();
@@ -13,24 +12,6 @@ var config = require('./config'),
 app.server = http.createServer(app);
 
 app.config = config;
-
-if (config.isMongoEnabled === true) {
-	console.log('connecting mongodb');
-	app.db = mongoose.createConnection(config.mongodb.uri);
-	app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
-	app.db.once('open', function() {
-	});
-	require('./models')(app, mongoose);
-	app.use(session({
-		resave: true,
-		saveUninitialized: true,
-		secret: config.cryptoKey,
-		store: new mongoStore({ url: config.mongodb.uri })
-	}));
-}
-
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- config.js
+++ config.js
@@ -1,6 +1,1 @@
 exports.port = 3000;
-exports.cryptoKey = 'a43gtbv';
-exports.isMongoEnabled = false;
-exports.mongodb = {
-	uri : 'localhost/SimpleDB'
-};
